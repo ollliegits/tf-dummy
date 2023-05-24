991e05271d67ef21a8916ddd8c381ac53b9a8975
ollliegits <48243036+ollliegits@users.noreply.github.com>
2023-05-24 14:24:49 +0200

    fix: Missing variables added.

--- variables.tf
+++ variables.tf
@@ -1,10 +1,29 @@
+variable "subscription_id" {
+  type        = string
+  description = "subscription id of azure subscription"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Tenant Id of Azure Tenant"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Service Principal Client Id"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Service Principal Client secret"
+}
+
 variable "name" {
   type        = string
   description = "(Required) Azure resource group name"
 }
 
 variable "location" {
-  default     = "East US"
   type        = string
   description = "(Required) Azure location name e.g. East US"
 }
